7d111b9ce5dcbef5db415c19350061b26f976fdb
Smrecz <diegoxlt@gmail.com>
2023-10-02 10:45:06 +0200

    Sort out chains 2

--- extensions/walletProviders/evm/src/consts.ts
+++ extensions/walletProviders/evm/src/consts.ts
@@ -11,15 +11,15 @@ export const DERIVATION_PATHS = new Map<Network, string>([
   [Network.ARBITRUM_NOVA, EVM_DERIVATION_PATH_COMMON],
   [Network.ARBITRUM_ONE, EVM_DERIVATION_PATH_COMMON],
   [Network.AURORA, EVM_DERIVATION_PATH_COMMON],
-  [Network.AVALANCHE_C, EVM_DERIVATION_PATH_COMMON],
+  [Network.AVALANCHE_C, "m/44'/9005'/0'/0"],
   [Network.BINANCE_SMART_CHAIN, EVM_DERIVATION_PATH_COMMON],
   [Network.CELO, "m/44'/52752'/0'/0"],
   [Network.CHILIZ, "m/44'/2182'/0'/0"],
-  [Network.CRONOS, EVM_DERIVATION_PATH_COMMON], //TODO: missing
+  [Network.CRONOS, "m/44'/394'/0'/0"],
   [Network.ETHEREUM, EVM_DERIVATION_PATH_COMMON],
   [Network.ETHEREUM_CLASSIC, "m/44'/61'/0'/0"],
   [Network.FANTOM, "m/44'/1007'/0'/0"],
-  [Network.FLARE, EVM_DERIVATION_PATH_COMMON], //TODO: missing
+  [Network.FLARE, EVM_DERIVATION_PATH_COMMON],
   [Network.GNOSIS, "m/44'/700'/0'/0"],
   [Network.HAQQ, "m/44'/1348'/0'/0"],
   [Network.HARMONY_ONE_SHARD_0, "m/44'/1023'/0'/0"],
@@ -27,8 +27,8 @@ export const DERIVATION_PATHS = new Map<Network, string>([
   [Network.KLAYTN, "m/44'/8217'/0'/0"],
   [Network.KUCOIN, EVM_DERIVATION_PATH_COMMON],
   [Network.OASIS, "m/44'/474'/0'/0'"],
-  [Network.OPTIMISM, EVM_DERIVATION_PATH_COMMON], //TODO: missing
-  [Network.PALM, EVM_DERIVATION_PATH_COMMON], //TODO: missing
+  [Network.OPTIMISM, EVM_DERIVATION_PATH_COMMON],
+  [Network.PALM, EVM_DERIVATION_PATH_COMMON],
   [Network.POLYGON, "m/44'/966'/0'/0"],
   [Network.VECHAIN, "m/44'/818'/0'/0"],
   [Network.XDC, "m/44'/550'/0'/0"]
