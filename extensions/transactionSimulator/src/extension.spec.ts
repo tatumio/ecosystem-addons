771f13d03f284fcae50c0acad3b30c27be546f5f
Smrecz <diegoxlt@gmail.com>
2023-10-25 22:43:07 +0200

    ALL-2876 - Transaction Simulation Extension - better tests

--- extensions/transactionSimulator/src/extension.spec.ts
+++ extensions/transactionSimulator/src/extension.spec.ts
@@ -4,126 +4,50 @@ import BigNumber from 'bignumber.js'
 import { TransactionSimulator } from './extension'
 import { TokenTransferDetail } from './types'
 
-describe('Transaction Simulator - Ethereum', () => {
-  let tatumSdk: BaseEvmClass
-
-  beforeAll(async () => {
-    tatumSdk = await TatumSDK.init({
-      network: Network.ETHEREUM,
-      configureExtensions: [TransactionSimulator],
-      verbose: true,
-    })
-  })
-
-  afterAll(async () => {
-    await tatumSdk.destroy()
-  })
-
-  describe('simulateTransfer', () => {
-    it('should simulate native transaction', async () => {
-      const fromAddress = '0xDce92f40cAdDE2C4e3EA78b8892c540e6bFe2f81'
-      const toAddress = '0x0Ae9E7437092BB7E7Bd6Eccf0eF1ad05591f5B47'
-      const value = 10000
-
-      const txRequest = {
-        from: fromAddress,
-        to: toAddress,
-        value: value,
-      }
-      const result = await tatumSdk.extension(TransactionSimulator).simulateTransfer(txRequest)
-      expect(result).toEqual({
-        transactionDetails: {
-          from: fromAddress,
-          to: toAddress,
-          value: value,
-          gasLimit: expect.any(Number),
-          gasPrice: expect.any(Number),
-        },
-        status: 'success',
-        balanceChanges: {
-          [fromAddress]: {
-            from: expect.any(BigNumber),
-            to: expect.any(BigNumber),
-          },
-          [toAddress]: {
-            from: expect.any(BigNumber),
-            to: expect.any(BigNumber),
-          },
-        },
-      })
-      expect(
-        result.balanceChanges[fromAddress].from.toNumber() - result.balanceChanges[fromAddress].to.toNumber(),
-      ).toBeGreaterThanOrEqual(value)
-      expect(
-        result.balanceChanges[toAddress].to.toNumber() - result.balanceChanges[toAddress].from.toNumber(),
-      ).toBe(value)
-    })
-    describe('simulateTransferErc20', () => {
-      it('should simulate token transaction', async () => {
-        const fromAddress = '0xDce92f40cAdDE2C4e3EA78b8892c540e6bFe2f81'
-        const toAddress = '0xaf758da9f7bdaa7590175193388e9c99427cc2d2'
-        const value = 420
-        const tokenContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-        const txRequest = {
-          from: fromAddress,
-          to: toAddress,
-          value: value,
-          tokenContractAddress: tokenContractAddress,
-        }
-        const result = await tatumSdk.extension(TransactionSimulator).simulateTransferErc20(txRequest)
-        expect(result).toEqual({
-          transactionDetails: {
-            from: fromAddress,
-            to: toAddress,
-            data: '0xa9059cbb000000000000000000000000af758da9f7bdaa7590175193388e9c99427cc2d2000000000000000000000000000000000000000000000000000000001908b100',
-            gasLimit: expect.any(Number),
-            gasPrice: expect.any(Number),
-            tokenContractAddress: tokenContractAddress,
-          },
-          status: 'success',
-          balanceChanges: {
-            [fromAddress]: {
-              from: expect.any(BigNumber),
-              to: expect.any(BigNumber),
-            },
-          },
-          tokenTransfers: {
-            [tokenContractAddress]: {
+const testData = [{
+    network: Network.ETHEREUM,
+    native: {
+        fromAddress: '0xDce92f40cAdDE2C4e3EA78b8892c540e6bFe2f81',
+        toAddress: '0x0Ae9E7437092BB7E7Bd6Eccf0eF1ad05591f5B47'
+    },
+      token: {
+          fromAddress: '0xDce92f40cAdDE2C4e3EA78b8892c540e6bFe2f81',
+          toAddress: '0xaf758da9f7bdaa7590175193388e9c99427cc2d2',
+          tokenContractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+          data: '0xa9059cbb000000000000000000000000af758da9f7bdaa7590175193388e9c99427cc2d200000000000000000000000000000000000000000000000000000000001e8480',
+          details: {
               name: 'TetherUSD',
               symbol: 'USDT',
               decimals: 6,
-              [fromAddress]: {
-                from: expect.any(BigNumber),
-                to: expect.any(BigNumber),
-              },
-              [toAddress]: {
-                from: expect.any(BigNumber),
-                to: expect.any(BigNumber),
-              },
-            },
-          },
-        })
-        expect(
-          (result.tokenTransfers[tokenContractAddress][fromAddress] as TokenTransferDetail).from
-            .minus((result.tokenTransfers[tokenContractAddress][fromAddress] as TokenTransferDetail).to)
-            .toNumber(),
-        ).toBe(value)
-        expect(
-          (result.tokenTransfers[tokenContractAddress][toAddress] as TokenTransferDetail).to
-            .minus((result.tokenTransfers[tokenContractAddress][toAddress] as TokenTransferDetail).from)
-            .toNumber(),
-        ).toBe(value)
-      })
-    })
-  })
-})
+          }
+      }
+  },
+    {
+        network: Network.CHILIZ,
+        native: {
+            fromAddress: '0x72917f88100893b3ba5a3d338c9176621ac3a01d',
+            toAddress: '0x3a7927bf4870309efc957b30ef9a655292369515'
+        },
+        token: {
+            fromAddress: '0xaaADB5a0bD733CDb9c1f61Be20Ed648D2809b476',
+            toAddress: '0xce04d9bcfe631fdfc1ba23c42b5c72a2f2c3872d',
+            tokenContractAddress: '0xd1723eb9e7c6ee7c7e2d421b2758dc0f2166eddc',
+            data: '0xa9059cbb000000000000000000000000ce04d9bcfe631fdfc1ba23c42b5c72a2f2c3872d0000000000000000000000000000000000000000000000000000000000000002',
+            details: {
+                name: 'Flamengo',
+                symbol: 'MENGO',
+                decimals: 0,
+            }
+        }
+    }
+]
 
-describe('Transaction Simulator - Chiliz', () => {
+describe.each(testData)('Transaction Simulator - $network', (testData) => {
   let tatumSdk: BaseEvmClass
 
   beforeAll(async () => {
     tatumSdk = await TatumSDK.init({
-      network: Network.CHILIZ,
+      network: testData.network,
       configureExtensions: [TransactionSimulator],
       verbose: true,
     })
@@ -134,50 +58,51 @@ describe('Transaction Simulator - Chiliz', () => {
   })
 
   describe('simulateTransfer', () => {
-    it('should simulate native transaction', async () => {
-      const fromAddress = '0x72917f88100893b3ba5a3d338c9176621ac3a01d'
-      const toAddress = '0x3a7927bf4870309efc957b30ef9a655292369515'
-      const value = 10000
+      it('should simulate native transaction', async () => {
+          const fromAddress = testData.native.fromAddress
+          const toAddress = testData.native.toAddress
+          const value = 10000
 
-      const txRequest = {
-        from: fromAddress,
-        to: toAddress,
-        value: value,
-      }
-      const result = await tatumSdk.extension(TransactionSimulator).simulateTransfer(txRequest)
-      expect(result).toEqual({
-        transactionDetails: {
-          from: fromAddress,
-          to: toAddress,
-          value: value,
-          gasLimit: expect.any(Number),
-          gasPrice: expect.any(Number),
-        },
-        status: 'success',
-        balanceChanges: {
-          [fromAddress]: {
-            from: expect.any(BigNumber),
-            to: expect.any(BigNumber),
-          },
-          [toAddress]: {
-            from: expect.any(BigNumber),
-            to: expect.any(BigNumber),
-          },
-        },
+          const txRequest = {
+              from: fromAddress,
+              to: toAddress,
+              value: value,
+          }
+          const result = await tatumSdk.extension(TransactionSimulator).simulateTransfer(txRequest)
+          expect(result).toEqual({
+              transactionDetails: {
+                  from: fromAddress,
+                  to: toAddress,
+                  value: value,
+                  gasLimit: expect.any(Number),
+                  gasPrice: expect.any(Number),
+              },
+              status: 'success',
+              balanceChanges: {
+                  [fromAddress]: {
+                      from: expect.any(BigNumber),
+                      to: expect.any(BigNumber),
+                  },
+                  [toAddress]: {
+                      from: expect.any(BigNumber),
+                      to: expect.any(BigNumber),
+                  },
+              },
+          })
+          expect(
+              result.balanceChanges[fromAddress].from.minus(result.balanceChanges[fromAddress].to).toNumber(),
+          ).toBeGreaterThanOrEqual(value)
+          expect(
+              result.balanceChanges[toAddress].to.minus(result.balanceChanges[toAddress].from).toNumber(),
+          ).toBe(value)
       })
-      expect(
-        result.balanceChanges[fromAddress].from.minus(result.balanceChanges[fromAddress].to).toNumber(),
-      ).toBeGreaterThanOrEqual(value)
-      expect(
-        result.balanceChanges[toAddress].to.minus(result.balanceChanges[toAddress].from).toNumber(),
-      ).toBe(value)
-    })
+  })
     describe('simulateTransferErc20', () => {
       it('should simulate token transaction', async () => {
-        const fromAddress = '0xaaADB5a0bD733CDb9c1f61Be20Ed648D2809b476'
-        const toAddress = '0xce04d9bcfe631fdfc1ba23c42b5c72a2f2c3872d'
+        const fromAddress = testData.token.fromAddress
+        const toAddress = testData.token.toAddress
         const value = 2
-        const tokenContractAddress = '0xd1723eb9e7c6ee7c7e2d421b2758dc0f2166eddc'
+        const tokenContractAddress = testData.token.tokenContractAddress
         const txRequest = {
           from: fromAddress,
           to: toAddress,
@@ -189,7 +114,7 @@ describe('Transaction Simulator - Chiliz', () => {
           transactionDetails: {
             from: fromAddress,
             to: toAddress,
-            data: '0xa9059cbb000000000000000000000000ce04d9bcfe631fdfc1ba23c42b5c72a2f2c3872d0000000000000000000000000000000000000000000000000000000000000002',
+            data: testData.token.data,
             gasLimit: expect.any(Number),
             gasPrice: expect.any(Number),
             tokenContractAddress: tokenContractAddress,
@@ -203,9 +128,9 @@ describe('Transaction Simulator - Chiliz', () => {
           },
           tokenTransfers: {
             [tokenContractAddress]: {
-              name: 'Flamengo',
-              symbol: 'MENGO',
-              decimals: 0,
+              name: testData.token.details.name,
+              symbol: testData.token.details.symbol,
+              decimals: testData.token.details.decimals,
               [fromAddress]: {
                 from: expect.any(BigNumber),
                 to: expect.any(BigNumber),
@@ -217,7 +142,6 @@ describe('Transaction Simulator - Chiliz', () => {
             },
           },
         })
-
         expect(
           (result.tokenTransfers[tokenContractAddress][fromAddress] as TokenTransferDetail).from
             .minus((result.tokenTransfers[tokenContractAddress][fromAddress] as TokenTransferDetail).to)
@@ -231,4 +155,3 @@ describe('Transaction Simulator - Chiliz', () => {
       })
     })
   })
-})
